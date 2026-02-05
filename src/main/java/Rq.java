0a1707252263565ffba53ac9e313e8275634cd35
perandowind <perandowind@gmail.com>
2026-02-05 12:47:22 +0900

    t4 b2

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -20,11 +20,9 @@ public class Rq {
 
         String param = cmd.split("\\?")[1];
         String paramKey = param.split("=")[0];
-        if (paramKey.equals("keyword")) {
-            return "자바";
-        }
-        if (paramKey.equals("keywordType")) {
-            return "title";
+        String paramValue = param.split("=")[1];
+        if (paramKey.equals(key)) {
+            return paramValue;
         }
 
         return "";
