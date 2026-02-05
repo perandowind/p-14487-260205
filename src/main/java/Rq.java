5f25e12f261faef975186b9cd8408f045901ea62
perandowind <perandowind@gmail.com>
2026-02-05 12:13:28 +0900

    t1 -blue

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -7,13 +7,12 @@ public class Rq {
     }
 
     public String getActionName() {
-        if(cmd.equals("삭제?id=1")){
-            return "삭제";
-        }
-        if(cmd.equals("수정?id=1")){
-            return "수정";
-        }
-
-        return "";
+//        if(cmd.equals("삭제?id=1")){
+//            return "삭제";
+//        }
+//        if(cmd.equals("수정?id=1")){
+//            return "수정";
+//        }
+        return cmd.split("\\?")[0];
     }
 }
