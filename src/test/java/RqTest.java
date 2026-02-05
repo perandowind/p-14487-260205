bac98ffd30e6cbe9a204271157bc91c6ae08b8ac
perandowind <perandowind@gmail.com>
2026-02-05 12:11:19 +0900

    t1 - green

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -6,12 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RqTest {
 
     @Test
-    @DisplayName(value = "rq.getActionName()")
+    @DisplayName(value = "rq.getActionName() - 삭제")
     void t1(){
 
         Rq rq = new Rq("삭제?id=1");
         String actionName = rq.getActionName();
         assertThat(actionName).isEqualTo("삭제");
     }
+
+    @Test
+    @DisplayName(value = "rq.getActionName() - 수정")
+    void t2(){
+
+        Rq rq = new Rq("수정?id=1");
+        String actionName = rq.getActionName();
+        assertThat(actionName).isEqualTo("수정");
+    }
 }
-rq -t1 -red
